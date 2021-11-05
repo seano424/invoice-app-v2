ba37ac2c5c4856edce3c9494a8046dc545bb57d5
Sean OReilly <soreilly424@gmail.com>
2021-11-05 11:55:47 -0500

    added auth

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,34 @@
+import NextAuth from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    // ...add more providers here
+  ],
+  // this is the default below...
+  // theme: {
+  //   logo: 'https://something...',
+  //   brandColor: "#f4144",
+  //   colorScheme: 'auto'
+  // },
+  pages: {
+    signIn: '/auth/signin',
+  },
+  callbacks: {
+    async session({ session, token, user }) {
+      session.user.username = session.user.name
+        .split(' ')
+        .join('')
+        .toLowerCase()
+
+      session.user.uid = token.sub
+
+      return session
+    },
+  },
+})
